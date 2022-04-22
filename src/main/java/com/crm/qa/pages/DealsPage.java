874d609db290a40af6174d0c9ba17531fe874174
Parth Ghevariya <parthghevariyakobdi@gmai.com>
2022-04-22 09:13:03 -0400

    added code in deals.page

--- src/main/java/com/crm/qa/pages/DealsPage.java
+++ src/main/java/com/crm/qa/pages/DealsPage.java
@@ -1,7 +1,14 @@
 package com.crm.qa.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 import com.crm.qa.base.TestBase;
 
 public class DealsPage extends TestBase  {
+	
+	//Page Factory
+	@FindBy(xpath = "//div[text()='Deals']")
+	WebElement dealsLabel;
 
 }
